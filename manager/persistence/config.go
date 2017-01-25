00e5ced18f06af4086592e0b87b8ee3679fa8056
ArkBriar <arkbriar@gmail.com>
2017-01-25 13:26:47 +0800

    Replace create with auto migrate and add comments to pass golint

--- manager/persistence/config.go
+++ manager/persistence/config.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/jinzhu/gorm"
+
+	// import mysql, postgressql, sqlite drivers
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -24,14 +26,30 @@ const (
 	logFile = "/tmp/ssmgr-db.log"
 )
 
+// migrate(create) tables: users, admin_users, servers, services,
+// products, orders.
+func autoMigrate() {
+	if dialect == "mysql" {
+		db.InstantSet("gorm:table_options", "ENGINE=InnoDB")
+	}
+	tables := []interface{}{&Server{}, &User{}, &User{Role: "admin"}, &Service{}, &Product{}, &Order{}}
+	errs := db.AutoMigrate(tables...).GetErrors()
+	if errs != nil {
+		logrus.Panicf("Tables' migration failed: %s\n", errs)
+	}
+}
+
 func init() {
 	var err error
 	db, err = gorm.Open(dialect, dbName)
 	if err != nil {
 		logrus.Panicln(err)
 	}
-	initConfig()
-	createTablesIfNotExsit()
+	db.LogMode(true).SetLogger(log.New(mustOpen(logFile), "\r\n", 0))
+	// Set up connection pool
+	db.DB().SetMaxIdleConns(20)
+	db.DB().SetMaxOpenConns(50)
+	autoMigrate()
 }
 
 func mustOpen(filename string) *os.File {
@@ -42,29 +60,7 @@ func mustOpen(filename string) *os.File {
 	return file
 }
 
-func initConfig() {
-	db.LogMode(true).SetLogger(log.New(mustOpen(logFile), "\r\n", 0))
-	// Set up connection pool
-	db.DB().SetMaxIdleConns(20)
-	db.DB().SetMaxOpenConns(50)
-}
-
-// create tables: users, admin_users, servers, services,
-// products, orders.
-func createTablesIfNotExsit() {
-	if dialect == "mysql" {
-		db.InstantSet("gorm:table_options", "ENGINE=InnoDB")
-	}
-	defaults := []interface{}{&Server{}, &User{}, &User{Role: "admin"}, &Service{}, &Product{}, &Order{}}
-	for table := range defaults {
-		if !db.HasTable(table) {
-			if err := db.CreateTable(table).Error; err != nil {
-				logrus.Panicln(err)
-			}
-		}
-	}
-}
-
+// GetDB returns a clone of the current DB connection.
 func GetDB() *gorm.DB {
 	return db.New()
 }
